a1847d6ae1b4f617ca839bad7982d7a42739ef0c
joker-119 <joker119@exiled.host>
2025-11-21 23:20:02 -0800

    Moderation(?) 2

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -51,6 +51,7 @@ class Kernel extends HttpKernel
         ValidatePostSize::class,
         TrimStrings::class,
         ConvertEmptyStringsToNull::class,
+        CheckBan::class,
     ];
 
     /**
