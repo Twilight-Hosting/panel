56a5cf7ac78ff569c89fdb4dba03cbc8c86e58c2
notzerotwo_ <63092138+NotZer0Two@users.noreply.github.com>
2025-12-28 00:43:08 +0100

    Cool Feature

---
+++ resources/scripts/api/server/sendPowerAction.ts
@@ -0,0 +1,9 @@
+﻿import http from '@/api/http';
+
+export default (uuid: string, action: string): Promise<void> => {
+    return new Promise((resolve, reject) => {
+        http.post(`/api/client/servers/${uuid}/power?signal=${action}`)
+            .then(() => resolve())
+            .catch(reject);
+    });
+};
